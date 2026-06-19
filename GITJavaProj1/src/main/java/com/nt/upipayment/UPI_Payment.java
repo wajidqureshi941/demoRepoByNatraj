2cd7788f82df838c7823b4ce490be83487f04010
Wajid Qureshi <wajidqureshi941@gmail.com>
2026-06-19 16:42:01 +0530

    UPI_payment method created

---
+++ GITJavaProj1/src/main/java/com/nt/upipayment/UPI_Payment.java
@@ -0,0 +1,8 @@
+package com.nt.upipayment;
+
+public class UPI_Payment {
+
+	public String Payment(long phoneNo,double amount) {
+		return "UPI payment is done of "+amount+"to this "+phoneNo+" mobile number";
+	}
+}
