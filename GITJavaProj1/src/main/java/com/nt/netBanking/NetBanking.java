96d201f16859d010bc88f522edb8f52f33d048a1
Wajid Qureshi <wajidqureshi941@gmail.com>
2026-06-19 17:19:40 +0530

    payment through NetBanking

---
+++ GITJavaProj1/src/main/java/com/nt/netBanking/NetBanking.java
@@ -0,0 +1,8 @@
+package com.nt.netBanking;
+
+public class NetBanking {
+
+	public String payment(long accountNo,double amount) {
+		return "Amount of Rs."+amount+" will be transfer using NetBanking to this accountNO "+accountNo;
+	}
+}
