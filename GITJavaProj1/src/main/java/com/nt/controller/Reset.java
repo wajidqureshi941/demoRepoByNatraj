0ff9694bd99b08739ddf8a96f6479d4102c9b539
Wajid Qureshi <wajidqureshi941@gmail.com>
2026-07-09 00:03:28 +0530

    reset class add

---
+++ GITJavaProj1/src/main/java/com/nt/controller/Reset.java
@@ -0,0 +1,13 @@
+package com.nt.controller;
+
+import java.util.Date;
+
+public class Reset {
+
+	public void reset_Meth() {
+		System.out.println(new Date().toString());
+	}
+	public static void main(String[] args) {
+		new Reset().reset_Meth();
+	}
+}
